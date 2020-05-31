66fded53a1599c1f5017ca635e2bab4abc8e1424
izzipizzi <kircheiandri@gmail.com>
2020-05-31 12:16:49 +0300

    build

---
+++ build/precache-manifest.ce71e45661c4be82482b05d9c55a2f8a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9697e740c431937ced5e38f8709b3714",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7844dce8696eefd54cb9",
+    "url": "/static/css/main.d0ff9f11.chunk.css"
+  },
+  {
+    "revision": "78815f205b88690a04bf",
+    "url": "/static/js/2.102d78ef.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.102d78ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7844dce8696eefd54cb9",
+    "url": "/static/js/main.d43674eb.chunk.js"
+  },
+  {
+    "revision": "95c828eae6e50c590326",
+    "url": "/static/js/runtime-main.95026ab2.js"
+  },
+  {
+    "revision": "f355fde96debd8b247cc7f3b1892b7aa",
+    "url": "/static/media/logo.f355fde9.png"
+  }
+]);

0443e1d59b7d425c6497969dd1746dc72d162dfd
izzipizzi <kircheiandri@gmail.com>
2020-06-07 12:58:45 +0300

    fixed add filtering

---
+++ build/precache-manifest.de2f2399cacc12d03110606638531e6c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "967691e3a696229e2e638b7474ca2bfa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9347a7489da21209895",
+    "url": "/static/css/main.3c597fcc.chunk.css"
+  },
+  {
+    "revision": "5671a9b2196b5dd54717",
+    "url": "/static/js/2.91aeadb3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.91aeadb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9347a7489da21209895",
+    "url": "/static/js/main.f0035b3d.chunk.js"
+  },
+  {
+    "revision": "95c828eae6e50c590326",
+    "url": "/static/js/runtime-main.95026ab2.js"
+  },
+  {
+    "revision": "f355fde96debd8b247cc7f3b1892b7aa",
+    "url": "/static/media/logo.f355fde9.png"
+  }
+]);
